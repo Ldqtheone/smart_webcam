0f21cd5e81ffb01b319286c6ff9f4bc4370b0cbe
marinebijon28 <47433408+marinebijon28@users.noreply.github.com>
2021-01-19 16:50:47 +0100

    path absolu
    path absolu

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,17 @@
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Veuillez saisir le chemin de l'image :");
-        String str = sc.nextLine();
-        System.out.println("Vous avez saisi le chemin : " + str);
+        String filename = sc.nextLine();
+        System.out.println("Vous avez saisi le chemin : " + filename);
+        sc.close();
+        // path
+        Path currentWorkingDir = Paths.get("").toAbsolutePath();
+        System.out.println(currentWorkingDir + "/inception5h/tensorPics/" + filename);
     }
 }
