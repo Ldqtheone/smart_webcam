52c3a979bd8d8f947dc87fec5ea67ca2f2f95582
marinebijon28 <47433408+marinebijon28@users.noreply.github.com>
2021-01-19 15:46:51 +0100

    recovery path of picture
    recovery path of picture

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Veuillez saisir le chemin de l'image :");
+        String str = sc.nextLine();
+        System.out.println("Vous avez saisi le chemin : " + str);
     }
 }
