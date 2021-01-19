24487ec7c29bae2e3bb2955d77bee981ba13b301
marinebijon28 <47433408+marinebijon28@users.noreply.github.com>
2021-01-19 18:17:15 +0100

    cutting function
     cutting function

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,54 @@
-import java.io.File;
+import org.tensorflow.Tensor;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+    public static Path createPathFile(String filename) {
+        Path currentWorkingDir = Paths.get("").toAbsolutePath();
+        String[] tab = filename.split("\\.");
+        System.out.println(tab[0]);
+        if (tab[tab.length - 1].equals("jpg")) {
+            String file = currentWorkingDir + "/inception5h/tensorPics/" + filename;
+            Path pathfile = Paths.get(file);
+            return pathfile;
+        }
+        return null;
+    }
+
+    public static String recoveryLineCommand() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Veuillez saisir le chemin de l'image :");
         String filename = sc.nextLine();
-        System.out.println("Vous avez saisi le chemin : " + filename);
         sc.close();
-        // path
-        Path currentWorkingDir = Paths.get("").toAbsolutePath();
-        System.out.println(currentWorkingDir + "/inception5h/tensorPics/" + filename);
+        return filename;
+    }
+
+    public static void main(String[] args) {
+        TFUtils utils = new TFUtils();
+
+        // recovery line in the shell
+        String filename = recoveryLineCommand();
+
+        // path of File
+        Path pathfile = createPathFile(filename);
+        if (pathfile == null)
+            return ;
+        System.out.println(pathfile);
+
+        // convert picture in a byte
+        byte[] tabByte = null;
+        try {
+            tabByte = Files.readAllBytes(pathfile);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+       // if (tabByte != null) {
+      //      Tensor input = utils.byteBufferToTensor(tabByte);
+         //   System.out.println(input);
+       // }
     }
 }
