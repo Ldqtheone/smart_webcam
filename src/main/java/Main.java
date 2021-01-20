478f6fadcbe33d7674cf4b974793a60656919697
marinebijon28 <47433408+marinebijon28@users.noreply.github.com>
2021-01-20 11:44:03 +0100

    story_1 recovery line of label.txt
    story_1 recovery line of label.txt

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,7 +9,6 @@ public class Main {
     public static Path createPathFile(String filename) {
         Path currentWorkingDir = Paths.get("").toAbsolutePath();
         String[] tab = filename.split("\\.");
-        System.out.println(tab[0]);
         if (tab[tab.length - 1].equals("jpg")) {
             String file = currentWorkingDir + "/inception5h/tensorPics/" + filename;
             Path pathfile = Paths.get(file);
@@ -36,19 +35,35 @@ public class Main {
         Path pathfile = createPathFile(filename);
         if (pathfile == null)
             return ;
-        System.out.println(pathfile);
 
         // convert picture in a byte
         byte[] tabByte = null;
+        byte[] tabByteRef = null;
         try {
             tabByte = Files.readAllBytes(pathfile);
-
+            tabByteRef = Files.readAllBytes(Path.of(Paths.get("").toAbsolutePath() + "/inception5h/tensorflow_inception_graph.pb"));
         } catch (IOException e) {
             e.printStackTrace();
         }
-       // if (tabByte != null) {
-      //      Tensor input = utils.byteBufferToTensor(tabByte);
-         //   System.out.println(input);
-       // }
+        if (tabByte != null && tabByteRef != null) {
+            Tensor input = utils.byteBufferToTensor(tabByte);
+            Tensor model = utils.executeModelFromByteArray(tabByteRef, input);
+            float[][] floats = new float[1][1008];
+            model.copyTo(floats);
+            System.out.println(floats[0][0]);
+
+
+            float max = 0.00000000000000F;
+            int line = 0;
+            for (int i = 0; i < 1001; i++) {
+                if (floats[0][i] > max) {
+                    max = floats[0][i];
+                    line = i + 1;
+                }
+            }
+            System.out.println(max);
+            System.out.println(line);
+            //   System.out.println(utils.executeSavedModel(String.valueOf(pathfile), input));
+        }
     }
 }
